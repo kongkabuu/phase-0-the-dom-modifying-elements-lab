1440b22486adc6f83774ea9ef89bec72a73d9e02
paul kabuu <kongkabuu9@gmail.com>
2023-03-12 00:46:20 +0300

    done assingmnet

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let main = document.querySelector("main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent ="PAUL-KABUU is the champion";
+document.body.append(newHeader);
+
+
